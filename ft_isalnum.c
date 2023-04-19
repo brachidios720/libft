0b64c4bec0e400320faeb039e226aa6d432309d2
brachidios720 <58665977+brachidios720@users.noreply.github.com>
2023-04-19 11:34:49 +0200

    Add files via upload

---
+++ ft_isalnum.c
@@ -0,0 +1,6 @@
+#include<ctype.h>
+
+int ft_isalnum(int c)
+{
+    return(ft_isalpha(c) || ft_isdigit(c));
+}
--- ft_strlen.c
+++ ft_strlen.c
@@ -1,21 +1,9 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strlen.c                                        :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: raphaelcarbonnel <raphaelcarbonnel@stud    +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/04/18 17:46:12 by raphaelcarb       #+#    #+#             */
-/*   Updated: 2023/04/18 17:58:13 by raphaelcarb      ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
+#include<stdio.h>
 
-#include "libft.h"
-
-size_t  ft_strlen(const char *s)
+size_t  strlen(const char *s)
 {
-    size_t     i;
-    
+    size_t i;
+
     i = 0;
     while(s[i] != '\0')
         i++;
